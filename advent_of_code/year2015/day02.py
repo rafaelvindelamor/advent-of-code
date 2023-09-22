bba9c86b3e5a00ae0a26cd136df7f6978be0d44f
Rafael Vindel Amor <rafael.vindel@gmail.com>
2023-09-22 08:25:32 +0200

    implement year2015/day02 part2 solution

--- advent_of_code/year2015/day02.py
+++ advent_of_code/year2015/day02.py
@@ -40,3 +40,46 @@ def wrapping_calculator_aux_part1(input: str) -> int:
 
 def wrapping_calculator_part1(input: list[str]) -> int:
     return sum(list(map(wrapping_calculator_aux_part1, input)))
+
+
+# --- Part Two ---
+# The elves are also running low on ribbon. Ribbon is all the same width, so
+# they only have to worry about the length they need to order, which they would
+# again like to be exact.
+#
+# The ribbon required to wrap a present is the shortest distance around its
+# sides, or the smallest perimeter of any one face. Each present also requires
+# a bow made out of ribbon as well; the feet of ribbon required for the perfect
+# bow is equal to the cubic feet of volume of the present. Don't ask how they
+# tie the bow, though; they'll never tell.
+#
+# For example:
+#
+# A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap
+# the present plus 2*3*4 = 24 feet of ribbon for the bow, for a total of 34
+# feet.
+# A present with dimensions 1x1x10 requires 1+1+1+1 = 4 feet of ribbon to wrap
+# the present plus 1*1*10 = 10 feet of ribbon for the bow, for a total of 14
+# feet.
+#
+# How many total feet of ribbon should they order?
+
+
+def ribbon_calculator_aux_part2(input: str) -> int:
+    dimensions = list(map(int, input.split("x")))
+    sorted_dimensions = sorted(dimensions)
+
+    length = dimensions[0]
+    width = dimensions[1]
+    height = dimensions[2]
+
+    face1 = 2 * sorted_dimensions[0]
+    face2 = 2 * sorted_dimensions[1]
+
+    volume = length * width * height
+
+    return face1 + face2 + volume
+
+
+def ribbon_calculator_part2(input: list[str]) -> int:
+    return sum(list(map(ribbon_calculator_aux_part2, input)))
--- tests/year2015/test_day02.py
+++ tests/year2015/test_day02.py
@@ -10,3 +10,13 @@ def test_examples_part1():
 
 def test_input_part1():
     assert sut.wrapping_calculator_part1(input.input.splitlines()) == 1588178
+
+
+def test_examples_part2():
+    assert sut.ribbon_calculator_part2(["2x3x4"]) == 34
+
+    assert sut.ribbon_calculator_part2(["1x1x10"]) == 14
+
+
+def test_input_part2():
+    assert sut.ribbon_calculator_part2(input.input.splitlines()) == 3783758
